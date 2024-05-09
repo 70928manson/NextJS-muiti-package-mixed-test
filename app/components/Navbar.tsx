3d12d7cc173f635f4ca20c5a43da20c461cca72c
70928manson <manson972@yahoo.com>
2024-05-09 15:42:10 +0800

    feat: 新增navbar component

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <div>
+      <ul className="flex justify-between m-10 item-center">
+        <div>
+            <Link href="/">
+                <li>
+                    首頁
+                </li>
+            </Link>
+        </div>
+        <div className="flex gap-10">
+            <Link href="/dashboard">
+                <li>
+                    後台
+                </li>
+            </Link>
+            <Link href="/login">
+                <li>
+                    登入
+                </li>
+            </Link>
+            <Link href="/signup">
+                <li>
+                    註冊
+                </li>
+            </Link>   
+        </div>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
